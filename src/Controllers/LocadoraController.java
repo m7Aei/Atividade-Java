1a2c07cd171d982a82291262d476e4c6763fac04
m7Aei <mateuspm2015@hotmail.com>
2019-08-22 17:47:32 -0300

    Criando o controller da locadora

---
+++ src/Controllers/LocadoraController.java
@@ -0,0 +1,14 @@
+package Controllers;
+
+import java.util.List;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import Database.ConnectionFactory;
+import Models.Locadora;
+
+public class LocadoraController {
+    
+}
